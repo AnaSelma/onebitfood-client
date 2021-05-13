4e5e0843937ce8420f50aeaa4c52737ebe5ea454
AnaSelma <anaselma1962@gmail.com>
2021-05-13 18:10:51 -0300

    create projects and component header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.scss'
+import Header from '../components/Header';
+import Container from 'react-bootstrap/Container';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>OnebitFood V2</title>
+        <link rel="icon" href="/favicon.ico" />  
+      </Head>
+      
+      <main>
+       <Header />
+       <Container className="mt-5">
+         <Component {...pageProps} />
+       </Container>
+     </main>
+    </>
+  )
+  
 }
 
 export default MyApp
