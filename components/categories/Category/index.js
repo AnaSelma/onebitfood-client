10795a7585b4ceb7a1a4804f87dd40a130044a94
AnaSelma <anaselma1962@gmail.com>
2021-05-15 14:45:34 -0300

    create categories lists

---
+++ components/categories/Category/index.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Category(props) {
+  return(
+    <div>
+      <Link href={`/restaurants?category=${props.title}`}>
+        <a>
+          <Image
+          src={props.image_url}
+          alt={props.title}
+          width={300}
+          height={200}
+          className='px-1 clickable_effect'
+          />
+        </a>
+      </Link>
+    </div>
+  )
+}
--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,8 +1,10 @@
 import ListRestaurants from '../../components/ListRestaurants';
+import Categories from '../../components/categories';
 
 export default function Restaurants() {
   return (
     <>
+      <Categories />
       <ListRestaurants/>
     </>
   )
